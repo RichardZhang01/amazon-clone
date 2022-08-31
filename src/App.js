097cc4be793771e91d86114446d912ad031e967b
RZhang <richardzhiyuanzhang@gmail.com>
2022-08-30 22:21:18 -0400

    install material-ui. restructure components and added pages directory. create Header component and styles.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import './App.css';
-import Home from './components/Home';
+import "./App.css";
+import Home from "./pages/Home";
+import Header from "./components/Header";
 
 function App() {
-    return (
-        <div className="App">
-            <Router>
-                <Routes>
-                    <Route 
-                        path="/"
-                        element={<Home />}
-                    />
-                </Routes>
-            </Router>
-        </div>
-    );
+  return (
+    <div className="App">
+        <Header />
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </Router>
+    </div>
+  );
 }
 
 export default App;
