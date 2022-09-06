732c1eed75d2f1f2d67fad910d0fd17f78c184a3
RZhang <richardzhiyuanzhang@gmail.com>
2022-09-06 14:06:27 -0400

    create product schema. create nest product modules, controller, and service, connect Product module to app module.

---
+++ api/src/product/product.controller.ts
@@ -0,0 +1,14 @@
+import { Body, Controller, Post } from '@nestjs/common';
+
+// localhost:3000/product
+@Controller('product')
+export class ProductController {
+//   @Post()
+//   createProduct(
+//     @Body('name') name: string,
+//     @Body('price') price: string,
+//     @Body('description') description?: string,
+//   ) {
+//     return this.ProductService
+//   }
+}
