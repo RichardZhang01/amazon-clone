300898924c742e206d3716b6a1fe760397598fb7
RZhang <richardzhiyuanzhang@gmail.com>
2022-09-10 22:13:53 -0400

    add login route with user validation and jwt.

--- api/src/auth/auth.controller.ts
+++ api/src/auth/auth.controller.ts
@@ -1,9 +1,12 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import { HttpStatus } from '@nestjs/common/enums';
+import { ExistingUserDTO } from 'src/user/dtos/existing-user.dto';
 
 import { NewUserDTO } from 'src/user/dtos/new-user.dto';
 import { UserData } from 'src/user/userData.interface';
 import { AuthService } from './auth.service';
 
+// localhost:3000/api/auth
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
@@ -11,5 +14,11 @@ export class AuthController {
   @Post('signup')
   signup(@Body() user: NewUserDTO): Promise<UserData | null> {
     return this.authService.signup(user);
+  }
+
+  @Post('login')
+  @HttpCode(HttpStatus.OK)
+  login(@Body() user: ExistingUserDTO): Promise<{token: string} | null> {
+    return this.authService.login(user);
   }
 }
