ea2292583bca285ae8c3086d7b3c86691ad27736
RZhang <richardzhiyuanzhang@gmail.com>
2022-09-15 14:42:44 -0400

    create redux store. create authslice, and respective models for the authSlice and global store.

---
+++ client/src/features/auth/authSlice.ts
@@ -0,0 +1,35 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { DisplayUser } from "./models/DisplayUser.interface";
+import { Jwt } from "./models/Jwt";
+
+interface AsyncState {
+  isLoading: boolean;
+  isSuccess: boolean;
+  isError: boolean;
+}
+
+interface AuthState extends AsyncState {
+  user?: DisplayUser | null;
+  jwt?: Jwt;
+  isAuthenticated?: boolean;
+}
+
+const initialState: AuthState = {
+  user: null,
+  jwt: null,
+  isAuthenticated: false,
+  isLoading: false,
+  isSuccess: false,
+  isError: false,
+};
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      //  REGISTER
+      .addCase();
+  },
+});
